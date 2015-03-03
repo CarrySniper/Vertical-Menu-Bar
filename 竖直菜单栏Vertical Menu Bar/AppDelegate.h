602ef567efaab81082bd2b05ebdb3b5e27035f92
cjq002 <692771080@qq.com>
2015-03-03 09:06:03 +0800

    Initial Commit

---
+++ 竖直菜单栏Vertical Menu Bar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  竖直菜单栏Vertical Menu Bar
+//
+//  Created by 陈家庆 on 15-3-3.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 竖直菜单栏Vertical Menu BarTests/_____Vertical_Menu_BarTests.m
@@ -0,0 +1,34 @@
+//
+//  _____Vertical_Menu_BarTests.m
+//  竖直菜单栏Vertical Menu BarTests
+//
+//  Created by 陈家庆 on 15-3-3.
+//  Copyright (c) 2015年 shikee. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _____Vertical_Menu_BarTests : XCTestCase
+
+@end
+
+@implementation _____Vertical_Menu_BarTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
